60d69d137723da6e93e243dfdec56fdcacac2456
fujii dan <2607dan@gmail.com>
2021-03-11 13:08:15 +0900

    ideasコントローラーリファクタリング

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,14 +1,15 @@
 class IdeasController < ApplicationController
 
   def search
-    if params[:category_name] && Category.find_by(name: params[:category_name])
-      @category_ideas = Category.joins(:ideas).where(name: params[:category_name]).select('ideas.id, name AS category, body')
-      render json: { data: @category_ideas}
-    elsif params[:category_name] && !Category.find_by(name: params[:category_name])
+    category_name = params[:category_name]
+    if category_name && Category.find_by(name: category_name)
+      category_ideas = select_ideas.where(name: category_name)
+      render json: { status: 200, message: "Ideas associated with categories", data: category_ideas}
+    elsif category_name && !Category.find_by(name: category_name)
       render json: { status: 404, message: "Category not found" }
     else
-      @category_ideas = Category.joins(:ideas).select('ideas.id, name AS category, body')
-      render json: { data: @category_ideas}
+      category_ideas = select_ideas
+      render json: { status: 200, message: "All ideas", data: category_ideas }
     end  
   end
   
@@ -27,4 +28,8 @@ class IdeasController < ApplicationController
   def idea_params
     params.require(:idea_category).permit(:name, :body)
   end
+
+  def select_ideas
+    Category.joins(:ideas).select('ideas.id, name AS category, body')
+  end  
 end
