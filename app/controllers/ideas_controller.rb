f1d01c90b066054001a419f430b346636b490e6a
fujii dan <2607dan@gmail.com>
2021-03-13 22:40:20 +0900

    Categoryモデルクラスメソッド定義

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,12 +3,12 @@ class IdeasController < ApplicationController
   def search
     category_name = params[:category_name]
     if category_name.present? && Category.find_by(name: category_name)
-      category_ideas = select_ideas.where(name: category_name)
+      category_ideas = Category.select_ideas.where(name: category_name)
       render status: 200, json: { status: 200, message: "Idea Search OK", data: category_ideas}
     elsif category_name.present? && !Category.find_by(name: category_name)
       render status: 404, json: { status: 404, message: "Category Not Found" }
     else
-      category_ideas = select_ideas
+      category_ideas = Category.select_ideas
       render status: 200, json: { status: 200, message: "OK", data: category_ideas }
     end
   end
@@ -27,8 +27,4 @@ class IdeasController < ApplicationController
   def idea_params
     params.require(:idea_category).permit(:name, :body)
   end
-
-  def select_ideas
-    Category.joins(:ideas).select('ideas.id, name AS category, body')
-  end  
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   validates :name, uniqueness: true
   has_many :ideas
+
+  def self.select_ideas
+    Category.joins(:ideas).select('ideas.id, name AS category, body')
+  end
 end
